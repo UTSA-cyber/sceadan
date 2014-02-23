db15c7ce38db704cd70b94bde7df01c6c27ddd1e
Simson Garfinkel <simsong@acm.org>
2014-02-22 23:17:43 -0500

    minor updates
    Former-commit-id: 5f3272a8b174aa516e6317b4bfab7b4b5c18e0c8

--- tools/sceadan_train.py
+++ tools/sceadan_train.py
@@ -33,33 +33,19 @@ def get_ftype(fn):
     if ftype.endswith("_SENT"): ftype="TBIRD"
     if ftype=="":
         m = between.search(fn)
+        if not m:
+            return ""
         ftype=m.group(1)
-    ftype = ftype_equivs.get(ftype,ftype)
-    return ftype
-    
-def process_file(outfile,fn):
-    ftype = get_ftype(fn)
-    fin = open(fn,'rb')
-    p1 = Popen([args.exe,'-b',str(args.blocksize),'-t',ftype,'-'],stdin=fin,stdout=PIPE)
-    outfile.write(p1.communicate()[0].decode('utf-8'))
-
-def ftype_from_name(fname):
-    ftype = os.path.splitext(fname)[1][1:].lower()
     return ftype_equivs.get(ftype,ftype)
+    
+def train_file(fname):
+    """Process a file and send a list of vectors to output file"""
 
-def process(fn_source,fname):
-    """Process a file and return a list of the vectors"""
-
-    if os.path.basename(fn_source)[0:1]=='.':
-        print("Ignoring dot file",fn_source)
+    if args.minfilesize and os.path.getsize(fname) < args.minfilesize:
+        print("Ignoring small file",fname)
         return
-
-    if args.minfilesize and os.path.getsize(fn_source) < args.minfilesize:
-        print("Ignoring small file",fn_source)
-        return
-
-    from subprocess import call,PIPE,Popen
-    ftype = ftype_from_name(fname)
+    
+    ftype = get_ftype(fname)
 
     # See if we need more of this type
     if block_count[ftype] > args.maxsamples: return
@@ -73,8 +59,14 @@ def process(fn_source,fname):
         return                  # no data, no problem (might be a sampling issue)
     vectors      = res[0].decode('utf-8').split('\n')
     offsets      = res[1].decode('utf-8').split('\n')
-    assert(len(vectors)==len(offsets))
+    if len(vectors)!=len(offsets):
+        print(offsets)
+        return
 
+    print("vectors=",vectors)
+    print("offsets=",offsets)
+    assert(len(vectors)==len(offsets))
+    
     # Finally, add to the file
     with open(args.outdir+"/"+ftype,"a") as f:
         for (v,o) in zip(vectors,offsets):
@@ -88,7 +80,51 @@ def process(fn_source,fname):
         file_count[ftype] += 1
         print("{} {} {}".format(fname,ftype,block_count[ftype]))
     
-def process_buf(outfile,fn,offset,bufsize):
+
+confusion={}
+def confusion_file(fname):
+    """Process a file and report how each block is classified"""
+    ftype = get_ftype(fname)
+    cmd = [args.exe,'-b',str(args.blocksize),fname]
+    p1  = Popen(cmd,stdout=PIPE,stderr=PIPE)
+    res = p1.communicate()
+    r   = re.compile("(\d+)\s+([^ ]+)")
+    for line in res[0].decode('utf-8').split('\n'):
+        m = r.search(line)
+        if not m: continue
+        btype = m.group(2)
+        if ftype not in confusion: confusion[ftype] = {}
+        if btype not in confusion[ftype]: confusion[ftype][btype] = 0
+        confusion[ftype][btype] += 1
+    print(confusion)
+
+
+def process_fname_dir(files,func):
+    print("pf = ",files)
+        
+def train_files():
+    for fname in args.files:
+        if os.path.isfile(fname): train_file(fname)
+        if os.path.isdir(fname):
+            for (dirpath,dirnames,filenames) in os.walk(fname):
+                for filename in filenames:
+                    if filename[0:1]=='.': continue # ignore dot files
+                    train_file(os.path.join(dirpath,filename))
+    print("Counts of each block type:")
+    for (ftype,count) in sorted(block_count.items()):
+        print("{:10} {:10}".format(ftype,count))
+    
+def confusion_files():
+    for fname in args.files:
+        if os.path.isfile(fname): confusion_file(fname)
+        if os.path.isdir(fname):
+            for (dirpath,dirnames,filenames) in os.walk(fname):
+                for filename in filenames:
+                    if filename[0:1]=='.': continue # ignore dot files
+                    confusion_file(os.path.join(dirpath,filename))
+
+
+def train_buf(outfile,fn,offset,bufsize):
     if args.skip:
         args.skip -= 1
         return
@@ -111,7 +147,7 @@ def run_grid():
     from distutils.spawn import find_executable
     train = find_executable('train')
     
-def verify_extract(outfile,fn):
+def verify_extract(fn):
     dname = os.path.dirname(fn)
     alerted = set()
     count = 0
@@ -122,20 +158,20 @@ def verify_extract(outfile,fn):
             if fn not in alerted:
                 alerted.add(fn)
                 print("Does not exist: {}".format(fn),file=sys.stderr)
-            continue
+                continue
         BLOCKSIZE = 512
-        process_buf(outfile,fn,int(offset)*BLOCKSIZE,BLOCKSIZE)
+        train_buf(outfile,fn,int(offset)*BLOCKSIZE,BLOCKSIZE)
         count += 1
         if count%1000==0:
             print("Processed {:,} blocks".format(count))
         
-
 if __name__=="__main__":
     import argparse,zipfile,os,time
     t0 = time.time()
     parser = argparse.ArgumentParser(description="Train sceadan from input files",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('files',action='store',help='input files for training. May be ZIP archives',nargs='?')
+    parser.add_argument('files',
+                        action='store',help='input files for training. May be ZIP archives',nargs='+')
     parser.add_argument('--blocksize',type=int,default=4096,help='blocksize for training.')
     parser.add_argument('--outfile',help='output file for combined vectors',default='vectors.train')
     parser.add_argument('--outdir',help='output dir for vector segments',default='vectors.train')
@@ -146,39 +182,23 @@ if __name__=="__main__":
     parser.add_argument('--maxsamples',help='Number of samples needed for each type',default=10000,type=int)
     parser.add_argument('--minfilesize',default=None,type=int)
     parser.add_argument('--j',help='specify concurrency factor',type=int,default=1)
-    parser.add_argument('--debug',action='store_true')
     parser.add_argument('--skip',type=int,help='Skip this many records (for testing)')
+    parser.add_argument('--confusion',action='store_true',help='Generate a confusion matrix')
+    parser.add_argument('--debug',action='store_true')
+
     args = parser.parse_args()
 
     t0 = time.time()
     outfile = open(args.outfile,'w')
 
     if args.verify:
-        verify_extract(outfile,args.verify)
-
-    # First create vectors from the inputs. Store the results in outfile
-    def process_file(fn):
-        if fn.lower().endswith('.zip'):
-            z = zipfile.ZipFile(fn)
-            for zfn in z.namelist():
-                if zfn.endswith("/"): continue
-                process(fn,zfn,iszipfile=True)
-        else:
-            process(fn,fn,iszipfile=False)
+        verify_extract(args.verify)
 
     if args.files:
-        for fn in args.files:
-            if os.path.isfile(fn):
-                process_file(fn)
-                continue
-            if os.path.isdir(fn):
-                for (dirpath,dirnames,filenames) in os.walk(fn):
-                    for filename in filenames:
-                        process_file(os.path.join(dirpath,filename))
-        
-    print("Counts of each block type:")
-    for (ftype,count) in sorted(block_count.items()):
-        print("{:10} {:10}".format(ftype,count))
-    
+        if args.confusion:
+            confusion_files()
+        else:
+            train_files()
+
     outfile.close()
     print("Elapsed time: {:10.2g} seconds".format(time.time()-t0))
