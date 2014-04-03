5d229fd36e5a0b971152e90b55c5cf7e170f1e98
Simson Garfinkel <simsong@acm.org>
2014-04-03 12:56:06 -0400

    precompiled model now mostly works (changed bigram ordering)
    Former-commit-id: a64e0c9c427ae9f96f5faabfe5ed003477a797d7

--- src/test.sh
+++ src/test.sh
@@ -8,10 +8,10 @@ bads=no
 
 doline() {
   while read line; do
-    v1=`echo $line|awk '{print $2;}'`
-    v2=`echo $line|sed 's:.*/::' | sed s/.txt//`
-    if [ $v1 != $v2 ]; then
-      echo bad line: $line
+    expected=`echo $line|sed 's:.*/::' | sed s/.txt// | tr '[a-z]' '[A-Z]'`
+    classified=`echo $line|awk '{print $2;}'`
+    if [ $expected != $classified ]; then
+      echo expected $expected got $classified line: $line
       bads=yes
     else
       echo good: $line
@@ -20,7 +20,8 @@ doline() {
 }
 
 
-./sceadan_app $srcdir/../testdata/good   | doline
+echo Good data here:
+./sceadan_app $srcdir/../data_test/good   | doline
 
 if [ $bads != "no" ]; then
   exit 1;
