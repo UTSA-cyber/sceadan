87604dc42886d64ba25a4035876a6f11ba57b4f6
Simson Garfinkel <simsong@acm.org>
2014-06-20 14:26:59 -0400

    now builds with own liblinear that gets included in .a file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Hopefully you checked out with git clone --recursive git@github.com:simsong/bulk_extractor.git
 
+(cd liblinear;sh bootstrap.sh)
 autoheader -f
 touch NEWS README AUTHORS ChangeLog
 touch stamp-h
---
+++ tools/test.sh
@@ -0,0 +1,2 @@
+/bin/rm -rf out-1
+python3 sceadan_train.py --data ../test_data --trainexe ../liblinear/train --exp out-1
